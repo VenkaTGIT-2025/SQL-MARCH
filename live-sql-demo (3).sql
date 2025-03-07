5bc0fe6e453b4339333f5357b0032d64a404a0ac
VenkaTGIT-2025 <venkataramakrishnav49@gmail.com>
2025-03-07 12:40:24 +0530

    Add files via upload

---
+++ live-sql-demo (3).sql
@@ -0,0 +1,121 @@
+--select * from dual
+
+-- string function
+
+--select first_name FROM hr.EMPLOYEES
+
+
+--select upper(FIRST_NAME),lower(FIRST_NAME), first_name FROM hr.EMPLOYEES
+
+--select upper('Data') from dual
+
+-- select upper('Data'),lower('DATA'), length('Data'),
+-- initcap('Data engineering'),
+-- ltrim('  Data')
+-- ,rtrim('data  ') from dual
+
+--select lpad('76445',5,'0') FROM dual
+
+--select rpad('786546',5,'0') FROM dual
+
+--select trim('   Tableau   ') from dual
+
+--select replace('data science','science','engineering') from dual
+
+--select replace('venkata ramakrishna','venkata','radhakrishna') from dual
+
+
+--SELECT round(873454,-1)from dual
+
+
+-- SELECT round(12346,-1)from dual
+
+-- SELECT round(12354,-1)from dual
+
+--SELECT round(12343,-2)from dual
+
+--SELECT round(12.3456,2)from DUAL
+
+--SELECT round(12.3456,-1)from DUAL
+
+--SELECT round(1480.3456,-3)from DUAL
+
+
+
+ --SELECT round(72.5564,2)from DUAL
+
+-- select sin(0)from dual
+-- select cos(0)from dual
+-- select tan(0)from dual
+-- select sin(90)from dual
+
+--select sysdate from dual
+
+--select current_timestamp from dual
+
+-- select upper('Tableau'),lower('Tableau'),length('Tableau '),
+-- initcap('Tableau engineering'),ltrim(' Tableau '),rtrim(' Tableau ')
+-- from dual
+
+--select lpad('342',5,'0') from dual
+-- select rpad('176486',5,'0') from dual
+-- select trim('   Tableau   ') from dual
+-- select replace('data Tableau','Tableau','venkataramakrishna') from dual
+
+
+
+
+-- select floor(6.4) from dual
+
+-- select floor(6.7) from dual
+
+
+-- select round(92.3654,2)from dual
+
+-- select round(92.3654,3)from dual
+
+
+
+-- select round(62.789,0) from dual 
+
+-- select round(452343,-2) from dual 
+
+
+
+
+
+--SELECT ABS(-40) AS Absolute_Value FROM DUAL; 
+
+-- SELECT CEIL(82.4) AS Rounded_Up FROM DUAL; 
+
+-- SELECT CEIL(192.8) AS Rounded_Up FROM DUAL; 
+
+
+-- SELECT FLOOR(812.7) AS Rounded_Down FROM DUAL; 
+
+-- SELECT FLOOR(812.3) AS Rounded_Down FROM DUAL; 
+
+
+--SELECT ROUND(1872.344, 2) AS Rounded_Value FROM DUAL; 
+
+ --SELECT TRUNC(812.346, 2) AS Truncated_Value FROM DUAL; 
+
+--SELECT MOD(200, 3) AS Remainder FROM DUAL; 
+
+---SELECT POWER(5, 3) AS Power_Value FROM DUAL; 
+
+--SELECT SQRT(225) AS Square_Root FROM DUAL; 
+
+--SELECT SIGN(-715) AS Sign_Value FROM DUAL; 
+
+--SELECT EXP(2) AS Exponential_Value FROM DUAL; 
+
+-- SELECT LN(20) AS Natural_Log FROM DUAL; 
+
+--SELECT LOG(20, 2000) AS Logarithm_Value FROM DUAL; 
+
+--select greatest(120,210,930,540,50,790) from dual
+
+--select least(170,209,130,65) from dual
+
+---select cos(0)FROM DUAL
